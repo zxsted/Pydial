8b5e9bc13f2dd59469747eae8f2d913b5d9cb6ae
tan <tanbowen@sjtu.edu.cn>
2018-01-24 23:34:32 +0800

    results59 updated.

--- results59/plot.sh
+++ results59/plot.sh
@@ -2,6 +2,5 @@ python ../pydial.py plot \
 _benchmark_gp_logs/*train* \
 _benchmarklogs/*train* \
 _madqn_tau_nodrop_4020_250100_logs/*train* \
-_madqn_tau_nodrop_logs/*train* \
-# _madqn_tau_nodrop_4020_0101_250100_0101_logs/*train* 
+_madqn_tau_nodrop_323232_128128128_logs/*train* 
 
