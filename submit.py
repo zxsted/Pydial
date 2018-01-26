2b33ccae9d2d711cb3e86de10e8ced86c4d45ba9
tan <tanbowen@sjtu.edu.cn>
2018-01-26 23:43:00 +0800

    submit.py updated.

--- submit.py
+++ submit.py
@@ -17,10 +17,25 @@ def submit(seed):
     runfile.write('/mnt/speechlab/users/bwt09/packages/anaconda2/bin/python pydial.py train '
                   'config/other_configs/' + sys.argv[1] + '.cfg --seed=' + str(seed) + '\n')
     while True:
-        print('sbatch -p cpuq -n 2 -o ' + sys.argv[1] + '_log/LOG' + str(seed) + ' run' + str(seed) + '.sh')
+        os.system('sbatch -p cpuq -n 2 -o ' + sys.argv[1] + '_log/LOG' + str(seed) + ' run' + str(seed) + '.sh')
         result_file = open(sys.argv[1] + '_log/LOG' + str(seed), 'r')
-        if len(result_file.readlines()) >= 420:
+
+        complete = False
+        error = False
+        for u in result_file:
+            if u.find('Training complete') != -1:
+                complete = True
+                break
+            if u.find('I/O Error') != -1:
+                error = True
+                break
+        if error:
+            continue
+        elif complete:
             break
+        else:
+            time.sleep(10)
+
 
     print(sys.argv[1], 'seed', str(seed), 'finished')
     print(finished, 'finished')
